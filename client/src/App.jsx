f2d63917a75bfa0b11d09d3b6f2a9d9dd69a2fd2
kwing25 <kendracwing@icloud.com>
2022-10-07 16:49:31 -0600

    Added server code. Added content to client. Working on project...

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Home from "./pages/Home";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Home />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+// Parse the body text
+app.use(bodyParser.json());
