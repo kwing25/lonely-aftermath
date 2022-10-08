47f1a4888867d81f53dc8bed8b05785e51674235
kwing25 <kendracwing@icloud.com>
2022-10-08 16:56:53 -0600

    Working on client side...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,20 @@
-//import "./App.css";
+import React from 'react';
+// import { Switch, Route } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
+
 import "./App.scss";
 
 import Home from "./pages/Home";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 function App() {
-  
+
   return (
     <div className="App">
       <Nav />
       <Home />
+      <Footer />
     </div>
   );
 }
